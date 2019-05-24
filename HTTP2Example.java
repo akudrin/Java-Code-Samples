95a02781876d35f6f4461bf929b078b919da1c98
Andrei Kudrin <andreikudrin@gmail.com>
2019-05-24 10:26:09 +0100

    HTTP2 example of a GET request that prints the response body as a String

---
+++ HTTP2Example.java
@@ -0,0 +1,16 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class HTTP2Example {
+
+    public static void main(String[] args) {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://www.google.com/")).build();
+        client.sendAsync(request, BodyHandlers.ofString()).thenApply(HttpResponse::body).thenAccept(System.out::println)
+                .join();
+    }
+}
