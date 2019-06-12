8491c449dc84decc2208b8aecf13b3024cb60a5e
Andrei Kudrin <andreikudrin@gmail.com>
2019-06-12 10:28:09 +0100

    object oriented java - Defining a Template for Classes to Extend

---
+++ ObjectOrientedJava/Schedule.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+public abstract class Schedule {
+    
+    public String scheduleYear;
+    public String teamName;
+    
+    public List<Team> teams;
+    
+    public Map<Team, LocalDate> gameMap;
+    
+    public Schedule(){}
+    
+    public Schedule(String teamName){
+        this.teamName = teamName;
+    }
+    
+    abstract void calculateDaysPlayed(int month);
+    
+}
---
+++ ObjectOrientedJava/TeamSchedule.java
@@ -0,0 +1,24 @@
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TeamSchedule extends Schedule {
+
+    public TeamSchedule(String teamName) {
+        super(teamName);
+    }
+
+    @Override
+    void calculateDaysPlayed(int month) {
+        int totalGamesPlayedInMonth = 0;
+        for (Map.Entry<Team, LocalDate> entry : gameMap.entrySet()) {
+            if (entry.getKey().equals(teamName)
+                    && entry.getValue().getMonth().equals(month)) {
+                totalGamesPlayedInMonth++;
+            }
+        }
+        System.out.println("Games played in specified month: " + totalGamesPlayedInMonth);
+    }
+
+}
