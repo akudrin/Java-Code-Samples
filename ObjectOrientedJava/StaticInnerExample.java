5cfa96059d98d493cc599691bee5ca6631c8a575
Andrei Kudrin <andreikudrin@gmail.com>
2019-06-12 10:33:07 +0100

    object oriented java - Increasing Class Encapsulation Inner Classes

---
+++ ObjectOrientedJava/StaticInnerExample.java
@@ -0,0 +1,26 @@
+public class StaticInnerExample {
+    
+    static String hello = "Hello";
+    
+    
+    public static void sayHello(){
+        System.out.println(hello);
+    }
+    
+    static class InnerExample {
+        String goodBye = "Good Bye";
+        
+        public void sayGoodBye(){
+            System.out.println(this.goodBye);
+        }
+    }
+    
+    public static void main (String[] args){
+        StaticInnerExample.sayHello();
+        StaticInnerExample.InnerExample inner =
+                new StaticInnerExample.InnerExample();
+        inner.sayGoodBye();
+    }
+}
+
+
