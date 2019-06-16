6ea655e2cdda613aa6ae54b8ff41fab13bfa290b
Andrei Kudrin <andreikudrin@gmail.com>
2019-06-16 16:19:36 +0100

    Lambdas - Implementing Runnable

---
+++ Lambdas/LambdaRunnable.java
@@ -0,0 +1,21 @@
+public class LambdaRunnable {
+
+    public static void main(String[] args) {
+
+        Runnable oldRunnable = new Runnable() {
+            @Override
+            public void run() {
+                int x = 5 * 3;
+                System.out.println("The variable using the old way equals: " + x);
+            }
+        };
+
+        Runnable lambdaRunnable = () -> {
+            int x = 5 * 3;
+            System.out.println("The variable using the lambda equals: " + x);
+        };
+
+        oldRunnable.run();
+        lambdaRunnable.run();
+    }
+}
