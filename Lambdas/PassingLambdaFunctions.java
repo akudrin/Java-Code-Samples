8864bea710e45466ec5b84a3e9dbbf5431aa9d03
Andrei Kudrin <andreikudrin@gmail.com>
2019-06-17 23:14:15 +0100

    Lambdas - Passing Lambda Expressions to Methods

---
+++ Lambdas/PassingLambdaFunctions.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+
+public class PassingLambdaFunctions {
+    /**
+     * Calculates a value based upon the calculation function that is passed
+     * in.
+     *  
+     */
+    public Double calculate(Function<List<Double>, Double> f1, 
+                                  Double [] args){
+        Double returnVal;
+        List<Double> varList = new ArrayList();
+        int idx = 0;
+        while (idx < args.length){
+            varList.add(args[idx]);
+            idx++;
+        }
+        returnVal=f1.apply(varList);
+        
+        return returnVal;
+    }
+}
