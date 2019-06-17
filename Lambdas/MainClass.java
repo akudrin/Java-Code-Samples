a82fa7ad6697216ecde59621d2cad8fce9225459
Andrei Kudrin <andreikudrin@gmail.com>
2019-06-17 10:08:45 +0100

    Lambdas - Accessing Class Variables from a Lambda Expression

---
+++ Lambdas/MainClass.java
@@ -0,0 +1,13 @@
+public class MainClass {
+    public static void main(String[] args){
+        System.out.println("==VariableAccess==");
+        VariableAccess va = new VariableAccess();
+        va.lambdaInMethod("Hello");
+        
+        System.out.println("==VariableAccessInner==");
+        VariableAccessInner vai = new VariableAccessInner();
+        VariableAccessInner.InnerClass inner = vai.new InnerClass();
+        inner.lambdaInMethod("Hello");
+        
+    }
+}
