bcb5e7ca4d52fee3218c6bdcb25db6046619c094
Andrei Kudrin <andreikudrin@gmail.com>
2019-07-03 13:40:09 +0100

    Exceptions and Logging - Managing Resources with try/catch Blocks

---
+++ Exceptions and Logging/Recipe9_6.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Recipe9_6 {
+    public static void main(String []args) {
+        Recipe9_6 recipe = new Recipe9_6();
+        recipe.start();
+    }
+
+    private void start() {
+        try (
+                FileOutputStream fos = new FileOutputStream("out.log");
+                BufferedOutputStream bos = new BufferedOutputStream(fos);
+                DataOutputStream dos = new DataOutputStream(bos)
+        ) {
+            dos.writeUTF("This is being written");
+            // If an exception occurs here, resources will be handled correctly
+            dos.close();
+        } catch (Exception e) {
+            System.out.println("Some bad exception happened ");
+        }
+    }
+}
