0a2d9e403b7ba9a50ed836151e8b11aab42223b2
Andrei Kudrin <andreikudrin@gmail.com>
2019-07-17 17:28:57 +0100

    JDBC - Obtaining Dates for Database Use

---
+++ JDBC/DatesTimes.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class DatesTimes {
+
+    static CreateConnection createConn = new CreateConnection();
+
+    public static void main(String[] args) {
+        insertRecord(
+                "Java 9 Recipes",
+                "APRESS");
+    }
+
+    private static void insertRecord(
+            String title,
+            String publisher) {
+        String sql = "INSERT INTO PUBLICATION VALUES("
+                + "NEXT VALUE FOR PUBLICATION_SEQ, ?,?,?,?)";
+        LocalDate pubDate = LocalDate.now();
+        try (Connection conn = createConn.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql);) {
+            pstmt.setInt(1, 100);
+            pstmt.setString(2, title);
+            pstmt.setDate(3,  java.sql.Date.valueOf(pubDate));
+            pstmt.setString(4, publisher);
+            pstmt.executeUpdate();
+            System.out.println("Record successfully inserted.");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
