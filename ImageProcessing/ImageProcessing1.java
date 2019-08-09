4a0686560c3de1691fef0251aa175dd25d5db191
Andrei Kudrin <andreikudrin@gmail.com>
2019-08-09 21:53:05 +0100

    ImageProcessing - Grey scale image conversion algorithm

--- ImageProcessing/ImageProcessing1.java
+++ ImageProcessing/ImageProcessing1.java
@@ -17,7 +17,22 @@ public class ImageProcessing1
         {
             System.out.println(e);
         }
-        
+        int width = img.getWidth();
+        int height = img.getHeight();
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                int p = img.getRGB(x,y);
+                int a = (p>>24)&0xff;
+                int r = (p>>16)&0xff;
+                int g = (p>>8)&0xff;
+                int b = p&0xff;
+                int avg = (r+g+b)/3;
+                p = (a<<24) | (avg<<16) | (avg<<8) | avg;
+                img.setRGB(x, y, p);
+            }
+        }
         try
         {
             f = new File("/Users/***/Desktop/out.jpg");
