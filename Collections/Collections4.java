5ee8124f0ba5a691207e0c4743635a9ee7720180
Andrei Kudrin <andreikudrin@gmail.com>
2019-08-19 07:47:19 +0100

    Collections - Partitioning and Grouping

---
+++ Collections/Collections4.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Collections4 {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("this", "is", "a", "long", "list", "of",
+                "strings", "to", "use", "as", "a", "demo");
+        Map<Boolean, List<String>> lengthMap = strings.stream()
+                .collect(Collectors.partitioningBy(s -> s.length() % 2 == 0));
+        lengthMap.forEach((key,value) -> System.out.printf("%5s: %s%n", key, value));
+
+        List<String> stringsA = Arrays.asList("this", "is", "a", "long", "list", "of",
+                "strings", "to", "use", "as", "a", "demo");
+        Map<Integer, List<String>> lengthMapA = stringsA.stream()
+                .collect(Collectors.groupingBy(String::length));
+        lengthMapA.forEach((k,v) -> System.out.printf("%d: %s%n", k, v));
+
+
+    }
+}
