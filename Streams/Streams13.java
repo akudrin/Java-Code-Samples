576da5296fd0790648914c3be53465b769695bd3
Andrei Kudrin <andreikudrin@gmail.com>
2019-08-21 09:45:56 +0100

    Collections - Streams of Random Numbers

---
+++ Streams/Streams13.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class Streams13 {
+    public static void main(String[] args) {
+        Random r = new Random();
+        r.ints(5)
+                .sorted()
+                .forEach(System.out::println);
+
+        r.doubles(5, 0, 0.5)
+                .sorted()
+                .forEach(System.out::println);
+
+        List<Long> longs = r.longs(5)
+                .boxed()
+                .collect(Collectors.toList());
+        System.out.println(longs);
+
+        List<Integer> listOfInts = r.ints(5, 10, 20)
+                .collect(LinkedList::new, LinkedList::add, LinkedList::addAll);
+        System.out.println(listOfInts);
+    }
+}
