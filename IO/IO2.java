2c45fd8b43b3c9cadcb019aae373ff59f7598d6d
Andrei Kudrin <andreikudrin@gmail.com>
2019-08-29 20:41:03 +0100

    IO - Retrieving Files as a Stream

---
+++ IO/IO2.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class IO2 {
+    public static void main(String[] args) {
+        try (Stream<Path> list = Files.list(Paths.get("src/main/java"))) {
+            list.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        
+    }
+}
