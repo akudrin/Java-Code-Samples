5d748e0e968006c7b08697230b3a4c6415bee68c
Andrei Kudrin <andreikudrin@gmail.com>
2019-09-09 14:58:10 +0100

    Concurency - Changing the pool size - Specifying the common pool size programmatically

---
+++ Concurrency/ParallelismConcurrency4.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.LongStream;
+
+public class ParallelismConcurrency4 {
+    public static void main(String[] args) {
+        System.setProperty(
+                "java.util.concurrent.ForkJoinPool.common.parallelism", "20");
+        long total = LongStream.rangeClosed(1, 3_000_000)
+                .parallel()
+                .sum();
+        int poolSize = ForkJoinPool.commonPool().getPoolSize();
+        System.out.println("Pool size: " + poolSize);
+
+        
+
+    }
+}
