d6318e8dcc50948e4cfd9ca66182570efd9f086c
Andrei Kudrin <andreikudrin@gmail.com>
2019-09-10 11:45:56 +0100

    Concurency - Changing the pool size - Creating your own ForkJoinPool

--- Concurrency/ParallelismConcurrency4.java
+++ Concurrency/ParallelismConcurrency4.java
@@ -1,8 +1,12 @@
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
 import java.util.stream.LongStream;
 
 public class ParallelismConcurrency4 {
     public static void main(String[] args) {
+
+        //Specifying the common pool size programmatically
         System.setProperty(
                 "java.util.concurrent.ForkJoinPool.common.parallelism", "20");
         long total = LongStream.rangeClosed(1, 3_000_000)
@@ -11,7 +15,25 @@ public class ParallelismConcurrency4 {
         int poolSize = ForkJoinPool.commonPool().getPoolSize();
         System.out.println("Pool size: " + poolSize);
 
-        
+        //Creating your own ForkJoinPool
+        ForkJoinPool pool = new ForkJoinPool(15);
+        ForkJoinTask<Long> task = pool.submit(
+                () -> LongStream.rangeClosed(1, 3_000_000)
+                        .parallel()
+                        .sum());
+        try {
+            total = task.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            pool.shutdown();
+        }
+        poolSize = pool.getPoolSize();
+        System.out.println("Pool size: " + poolSize);
+
+
+
+
 
     }
 }
