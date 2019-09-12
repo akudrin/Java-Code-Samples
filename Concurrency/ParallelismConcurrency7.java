b9bb3ae781fe6b7b7880ab9d6105e70e388d75b1
Andrei Kudrin <andreikudrin@gmail.com>
2019-09-12 09:45:47 +0100

    Concurency - Coordinating CompletableFutures - Running CompletableFuture tasks on a separate thread pool

--- Concurrency/ParallelismConcurrency7.java
+++ Concurrency/ParallelismConcurrency7.java
@@ -1,10 +1,22 @@
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ParallelismConcurrency7 {
     public static void main(String[] args) {
 
         //Coordinating tasks using a CompletableFuture
         CompletableFuture.supplyAsync(() -> new ParallelismConcurrency7().sleepThenReturnString())
+                .thenApply(Integer::parseInt)
+                .thenApply(x -> 2 * x)
+                .thenAccept(System.out::println)
+                .join();
+        System.out.println("Running...");
+
+        //Running CompletableFuture tasks on a separate thread pool
+        ExecutorService service = Executors.newFixedThreadPool(4);
+
+        CompletableFuture.supplyAsync(() -> new ParallelismConcurrency7().sleepThenReturnString(), service)
                 .thenApply(Integer::parseInt)
                 .thenApply(x -> 2 * x)
                 .thenAccept(System.out::println)
