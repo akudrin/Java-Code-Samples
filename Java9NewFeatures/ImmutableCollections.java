d80b168e316fb4e932cda838150555181c69f79e
Andrei Kudrin <andreikudrin@gmail.com>
2019-09-16 09:58:29 +0100

    Java 9 new features - Creating Immutable Collections - Immutable map from entries

--- Java9NewFeatures/ImmutableCollections.java
+++ Java9NewFeatures/ImmutableCollections.java
@@ -1,8 +1,12 @@
 import org.junit.Test;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ImmutableCollections {
     public static void main(String[] args) {
@@ -45,6 +49,39 @@ public class ImmutableCollections {
         assertEquals(1, holders.get(0).getX());
         holders.get(0).setX(4);
         assertEquals(4, holders.get(0).getX());
+    }
+
+    @Test
+    public void immutableMapFromEntries() throws Exception {
+        Map<String, String> jvmLanguages = Map.ofEntries(
+                Map.entry("Java", "http://www.oracle.com/technetwork/java/index.html"),
+                Map.entry("Groovy", "http://groovy-lang.org/"),
+                Map.entry("Scala", "http://www.scala-lang.org/"),
+                Map.entry("Clojure", "https://clojure.org/"),
+                Map.entry("Kotlin", "http://kotlinlang.org/"));
+        Set<String> names = Set.of("Java", "Scala", "Groovy", "Clojure", "Kotlin");
+        List<String> urls = List.of("http://www.oracle.com/technetwork/java/index.html",
+                "http://groovy-lang.org/",
+                "http://www.scala-lang.org/",
+                "https://clojure.org/",
+                "http://kotlinlang.org/");
+        Set<String> keys = jvmLanguages.keySet();
+        Collection<String> values = jvmLanguages.values();
+        names.forEach(name -> assertTrue(keys.contains(name)));
+        urls.forEach(url -> assertTrue(values.contains(url)));
+        Map<String, String> javaMap = Map.of("Java",
+                "http://www.oracle.com/technetwork/java/index.html",
+                "Groovy",
+                "http://groovy-lang.org/",
+                "Scala",
+                "http://www.scala-lang.org/",
+                "Clojure",
+                "https://clojure.org/",
+                "Kotlin",
+                "http://kotlinlang.org/");
+        javaMap.forEach((name, url) -> assertTrue(
+                jvmLanguages.keySet().contains(name) &&
+                jvmLanguages.values().contains(url)));
     }
 
 }
