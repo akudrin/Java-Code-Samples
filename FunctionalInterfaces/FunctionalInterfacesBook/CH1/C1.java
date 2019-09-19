3b9f0494f5585e5b208a09623b29fb8fb77ad7f9
Andrei Kudrin <andreikudrin@gmail.com>
2019-09-19 10:37:11 +0100

    FunctionalInterfacesBook - intro into java's interfaces

---
+++ FunctionalInterfaces/FunctionalInterfacesBook/CH1/C1.java
@@ -0,0 +1,7 @@
+public class C1 implements I1
+{
+    @Override 
+    public void method1() {}
+    @Override 
+    public String method2(String x) { return x; }
+}
---
+++ FunctionalInterfaces/FunctionalInterfacesBook/CH1/I1.java
@@ -0,0 +1,5 @@
+public interface I1
+{
+    void method1();
+    String method2(String x);
+}
