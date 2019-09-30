e2d546febfb71394bce4089462cbdf3070861b8f
Andrei Kudrin <andreikudrin@gmail.com>
2019-09-30 09:20:24 +0100

    FunctionalInterfacesBook - The Function.identity Method

---
+++ FunctionalInterfaces/FunctionalInterfacesBook/CH4/Sect4_Ex1.java
@@ -0,0 +1,9 @@
+import java.util.function.Function;
+public class Sect4_Ex1 
+{
+    public static void main(String[] args)
+    {
+        Function<String,String> f = Function.identity();
+        System.out.println(f.apply("HELLO WORLD"));
+    }
+}
