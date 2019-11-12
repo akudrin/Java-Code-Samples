4ad9247050e299ae553422a038292fb1e9a275f5
Andrei Kudrin <andreikudrin@gmail.com>
2019-11-12 15:07:48 +0000

    FunctionalInterfacesBook - Using the thenApply and thenAccept methods

---
+++ FunctionalInterfaces/FunctionalInterfacesBook/CH13/Sect5_Ex2.java
@@ -0,0 +1,12 @@
+import java.util.concurrent.CompletableFuture;
+public class Sect5_Ex2 
+{
+    public static void main(String[] args)
+    {
+        CompletableFuture
+         .supplyAsync( () -> "RED")              // CompletableFuture<String>
+         .thenApply(x -> 
+              x.equals("RED")? "GREEN":"BLUE")   // CompletableFuture<String>
+         .thenAccept(x -> System.out.println(x));// CompletableFuture<Void>
+    }
+}
