7dba4e0544c7f31bfd94f8e96b8e706f584b471f
Andrei Kudrin <andreikudrin@gmail.com>
2020-03-10 15:05:43 +0000

    Java Lang Features - A Test Case That Uses the TestCase Annotations

---
+++ Java Language Features/PolicyTestCases.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class PolicyTestCases {
+    // Must throw IOExceptionn
+    @TestCase(willThrow = IOException.class)
+    public static void testCase1() {
+        // Code goes here
+    }
+
+    // We are not expecting any exception
+    @TestCase()
+    public static void testCase2() {
+        // Code goes here
+    }
+}
