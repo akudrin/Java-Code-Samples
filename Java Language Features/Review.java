4a1d87ab1f8ae1c8140d84e36d8936062988cd8b
Andrei Kudrin <andreikudrin@gmail.com>
2020-03-11 16:09:27 +0000

    Java IO,NIO,NIO2 - An Annotation Type That Uses an enum Type Element

---
+++ Java Language Features/Review.java
@@ -0,0 +1,7 @@
+public @interface Review {
+    ReviewStatus status() default ReviewStatus.PENDING;
+    String comments() default "";
+    
+    // ReviewStatus enum is a member of the Review annotation type
+    public enum ReviewStatus {PENDING, FAILED, PASSED, PASSEDWITHCHANGES};
+}
