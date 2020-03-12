efd001e208e75913a0156222d0e346c1c9faf799
Andrei Kudrin <andreikudrin@gmail.com>
2020-03-12 14:46:15 +0000

    Java Lang Features - The Fatal and NonZero annotation types can be used wherever a type is used

---
+++ Java Language Features/CH1 - Annotations/Test1.java
@@ -0,0 +1,14 @@
+public class Test1 {
+    public void processData() throws @Fatal Exception {
+        double value = getValue();
+        int roundedValue = (@NonZero int) value;
+        Test t = new @Fatal Test();
+        // More code goes here
+    }
+
+    public double getValue() {
+        double value = 189.98;
+        // More code goes here
+        return value;
+    }
+}
