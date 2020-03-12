dd79bea9e365cd3f453400f2f07d239019ed2ca7
Andrei Kudrin <andreikudrin@gmail.com>
2020-03-12 14:56:07 +0000

    Java Lang Features - A Repeatable Annotation Type That Uses the ChangeLogs as the Containing Annotation Type

---
+++ Java Language Features/CH1 - Annotations/ChangeLog.java
@@ -0,0 +1,12 @@
+
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(ChangeLogs.class)
+public @interface ChangeLog {
+    String date();
+
+    String comments();
+}
