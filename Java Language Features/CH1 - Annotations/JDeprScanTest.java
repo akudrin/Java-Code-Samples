c70985a7908b6c0ff9d82f2e0ad0216ffac3df96
Andrei Kudrin <andreikudrin@gmail.com>
2020-03-13 14:23:30 +0000

    Java Lang Features - JDeprScanTest Class That Uses The Ordinarily Deprecated Method stop() (jdeprscan --list)

---
+++ Java Language Features/CH1 - Annotations/JDeprScanTest.java
@@ -0,0 +1,11 @@
+
+public class JDeprScanTest {
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> System.out.println("Test"));
+        t.start();
+        t.stop();
+        Thread t2 = new Thread(() -> System.out.println("Test"));
+        t2.start();
+        t2.destroy();
+    }
+}
