31c1e4a11d396bb19db2a2b96bb78b301816a78f
Andrei Kudrin <andreikudrin@gmail.com>
2020-03-16 16:14:14 +0000

    Java Lang Features - An Example of a Top-Level Class

---
+++ Java Language Features/CH2 - Inner Classes/TopLevel.java
@@ -0,0 +1,11 @@
+public class TopLevel {
+    private int value = 101;
+
+    public int getValue() {
+        return value;
+    }    
+
+    public void setValue (int value) {
+        this.value = value;
+    }
+}
