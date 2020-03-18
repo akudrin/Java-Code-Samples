42e949512b95808a6fc8c3d532ab97e898ff893a
Andrei Kudrin <andreikudrin@gmail.com>
2020-03-18 16:15:23 +0000

    Java Lang Features - An Anonymous Class Example

---
+++ Java Language Features/CH2 - Inner Classes/HelloAnonymous.java
@@ -0,0 +1,10 @@
+public class HelloAnonymous {
+    public static void main(String[] args) {
+        new Object() {
+            // An instance initializer
+            {
+                System.out.println("Hello from an anonymous class.");
+            }
+        }; // A semicolon is necessary to end the statement
+    }
+}
