f03e236d6648aa61b90b8f903771f50a7d6cf533
Andrei Kudrin <andreikudrin@gmail.com>
2020-03-19 08:15:30 +0000

    Java Lang Features - Changed RandomLocal class to have anonymous class

--- Java Language Features/CH2 - Inner Classes/RandomLocal.java
+++ Java Language Features/CH2 - Inner Classes/RandomLocal.java
@@ -1,19 +1,26 @@
 public class RandomLocal {
     public RandomInteger getRandomInteger() {
-        // A local inner class that inherits from the RandomInteger class 
+        // A local inner class that inherits from the RandomInteger class
         class RandomIntegerLocal extends RandomInteger {
             @Override
             public int getValue() {
-                // Get two random integers and return the average ignoring the 
-                // fraction part 
+                // Get two random integers and return the average ignoring the
+                // fraction part
                 long n1 = rand.nextInt();
                 long n2 = rand.nextInt();
 
-                int value = (int) ((n1 + n2)/2);
+                int value = (int) ((n1 + n2) / 2);
                 return value;
             }
         }
 
+        /*
+         * // Anonymous inner class that inherits from the RandomInteger class return
+         * new RandomInteger() { public int getValue() { // Get two random integers and
+         * return the average ignoring the fraction part long n1 = rand.nextInt(); long
+         * n2 = rand.nextInt(); int value = (int)((n1 + n2)/2); return value; } };
+         */
+
         return new RandomIntegerLocal();
-    } // End of the getRandomInteger() method 
+    } // End of the getRandomInteger() method
 }
