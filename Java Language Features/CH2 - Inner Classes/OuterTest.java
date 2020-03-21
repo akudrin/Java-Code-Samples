2d29924e959f4e82e2702ef6797f3f965f3bcaa5
Andrei Kudrin <andreikudrin@gmail.com>
2020-03-21 08:39:29 +0000

    Java Lang Features - Testing an Inner Class That Accesses the Instance Members of its Enclosing Class

---
+++ Java Language Features/CH2 - Inner Classes/OuterTest.java
@@ -0,0 +1,17 @@
+public class OuterTest {
+    public static void main(String[] args) {
+        Outer out = new Outer();
+        Outer.Inner in = out.new Inner();
+
+        // Print the value
+        out.printValue();
+        in.printValue();
+
+        // Set a new value
+        out.setValue(828);
+
+        // Print the value
+        out.printValue();
+        in.printValue();
+    }
+}
