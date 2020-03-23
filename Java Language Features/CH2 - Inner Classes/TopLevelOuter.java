aadab6957e64dab7fecb6b94f2c2fdc3d1e48492
Andrei Kudrin <andreikudrin@gmail.com>
2020-03-23 08:14:09 +0000

    Java Lang Features - Inner Classes and Variable Scopes

---
+++ Java Language Features/CH2 - Inner Classes/TopLevelOuter.java
@@ -0,0 +1,19 @@
+public class TopLevelOuter {
+    private int v1 = 100;
+
+    // Here, only v1 is in scope
+    public class InnerLevelOne {
+        private int v2 = 200;
+
+        // Here, only v1 and v2 are in scope
+        public class InnerLevelTwo {
+            private int v3 = 300;
+
+            // Here, only v1, v2, and v3 are in scope
+            public class InnerLevelThree {
+                private int v4 = 400;
+                // Here, all v1, v2, v3, and v4 are in scope
+            }
+        }
+    }
+}
