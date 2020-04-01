14e54a69af6ddf1098d8d721a025e90d79c0f1c2
Andrei Kudrin <andreikudrin@gmail.com>
2020-04-01 08:35:51 +0100

    Java Lang Features - A PublicPerson Class with a Public Name Field

---
+++ Java Language Features/CH3 - Reflection/PublicPerson.java
@@ -0,0 +1,12 @@
+public class PublicPerson {
+    private int id = -1;
+    public String name = "Unknown";
+
+    public PublicPerson() {
+    }
+
+    @Override
+    public String toString() {
+        return "Person: id=" + this.id + ", name=" + this.name;
+    }
+}
