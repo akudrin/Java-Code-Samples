6aac8e47ab460b6105cabc9debf26086c22290dc
Andrei Kudrin <andreikudrin@gmail.com>
2020-05-02 19:58:29 +0100

    Java Lang Features - An Uncaught Exception Handler for a Thread

---
+++ Java Language Features/CH6 - Threads/CatchAllThreadExceptionHandler.java
@@ -0,0 +1,6 @@
+public class CatchAllThreadExceptionHandler implements Thread.UncaughtExceptionHandler {
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        System.out.println("Caught Exception from Thread: " + t.getName());
+    }
+}
