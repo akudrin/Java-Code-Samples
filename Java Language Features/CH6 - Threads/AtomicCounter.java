bf6f8c310c191504555b7a348ba40fb5ea2e2ee6
Andrei Kudrin <andreikudrin@gmail.com>
2020-05-03 20:17:11 +0100

    Java Lang Features - A Counter Class Using Atomic Variable

---
+++ Java Language Features/CH6 - Threads/AtomicCounter.java
@@ -0,0 +1,9 @@
+import java.util.concurrent.atomic.AtomicLong;
+
+public class AtomicCounter {
+    private final AtomicLong value = new AtomicLong(0L);
+
+    public long next() {
+        return value.incrementAndGet();
+    }
+}
