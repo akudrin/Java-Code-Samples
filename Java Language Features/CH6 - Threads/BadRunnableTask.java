124dcd96bd7fe36cbe991c042eeb39543b5e5665
Andrei Kudrin <andreikudrin@gmail.com>
2020-05-17 08:16:44 +0100

    Java Features - Printing the Runtime Stack Trace from the execute() Method of the Executor

---
+++ Java Language Features/CH6 - Threads/BadRunnableTask.java
@@ -0,0 +1,14 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class BadRunnableTask {
+    public static void main(String[] args) {
+        Runnable badTask = () -> {
+            throw new RuntimeException("The task threw an exception...");
+        };
+
+        ExecutorService exec = Executors.newSingleThreadExecutor();
+        exec.execute(badTask);
+        exec.shutdown();
+    }
+}
