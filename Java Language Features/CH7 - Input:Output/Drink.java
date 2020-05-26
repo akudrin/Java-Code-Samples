db455c939a5e6fe3003443e1bb4f7747fb6ce1f5
Andrei Kudrin <andreikudrin@gmail.com>
2020-05-26 15:46:48 +0100

    Java Features - An Abstract Drink Class to Model the Abstract Component in the Decorator Pattern

---
+++ Java Language Features/CH7 - Input:Output/Drink.java
@@ -0,0 +1,12 @@
+public abstract class Drink {
+    protected String name;
+    protected double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
