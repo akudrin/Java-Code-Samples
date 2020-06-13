670aa64408f4633f9e2e0574d1375b9a7bbc8884
Andrei Kudrin <andreikudrin@gmail.com>
2020-06-13 21:39:19 +0100

    Java Features - A Dummy Output Stream Class That Will Swallow All Written Data

---
+++ Java Language Features/CH7 - Input:Output/DummyStandardOutput.java
@@ -0,0 +1,9 @@
+import java.io.OutputStream;
+import java.io.IOException;
+
+public class DummyStandardOutput extends OutputStream {
+    @Override
+    public void write(int b) throws IOException {
+        // Do not do anything. Swallow whatever is written
+    }
+}
