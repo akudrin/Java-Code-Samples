c2724098ffe5377f256159485320e695f12b9476
Andrei Kudrin <andreikudrin@gmail.com>
2020-07-20 21:02:58 +0100

    Java Features - A BigObject Class, Which Uses Big Memory

---
+++ Java Language Features/CH11 - Garbage Collection/BigObject.java
@@ -0,0 +1,23 @@
+public class BigObject {
+    // Declare a big array of 20 KB.
+    private final long[] anArray = new long[20480];
+
+    // Have an id to track the object
+    private final long id;
+
+    public BigObject(long id) {
+        this.id = id;
+    }
+
+    // Define finalize() to track the object’s finalization
+    @SuppressWarnings("deprecation")
+    @Override
+    public void finalize() {
+        System.out.println("finalize() called for id: " + id);
+    }
+
+    @Override
+    public String toString() {
+        return "BigObject: id = " + id;
+    }
+}
