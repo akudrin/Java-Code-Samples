c73290d5384f7fc4ed5370cc7cee1c8409177995
Andrei Kudrin <andreikudrin@gmail.com>
2020-08-29 10:21:18 +0100

    Java Features - Collecting Results into a Collection

---
+++ Java Language Features/CH13 - Streams/CollectTest.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CollectTest {
+    public static void main(String[] args) {
+        List<String> sortedNames = Person.persons()
+                                         .stream()
+                                         .map(Person::getName)
+                                         .sorted()
+                                         .collect(Collectors.toList());
+        System.out.println(sortedNames);
+    }
+}
